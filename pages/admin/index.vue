4315ae2637ec4cd7a9add69f0f8fca732257b379
fajarkusuma46 <fajarkusuma46@gmail.com>
2025-06-30 13:21:21 +0700

    refactor: improve CSS formatting and structure; update Card component usage in admin dashboard

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -3,16 +3,19 @@
     <h1 class="text-2xl font-bold text-slate-700">Dashboard Admin</h1>
     <!-- Content -->
     <div class="wrapper h-screen">
-      <div class="container mt-4">
+      <div class="container mt-6">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          <Card>
-            <CardHeader>
-              <CardTitle>Card Title</CardTitle>
-              <CardDescription>Card Description</CardDescription>
-            </CardHeader>
-            <CardContent> Card Content </CardContent>
-            <CardFooter> Card Footer </CardFooter>
-          </Card>
+          <div class="item">
+            <div class="title mb-4 font-bold text-primary">Indeks SPBE</div>
+            <Card>
+              <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+              </CardHeader>
+              <CardContent> Card Content </CardContent>
+              <CardFooter> Card Footer </CardFooter>
+            </Card>
+          </div>
         </div>
       </div>
     </div>
