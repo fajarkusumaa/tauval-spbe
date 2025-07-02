12ccb1ec1a0fd8c0c167b11e33a71df806e0b1d7
fajarkusuma46 <fajarkusuma46@gmail.com>
2025-07-02 09:32:24 +0700

    feat: add Label component and implement login page with form structure

--- pages/admin/konfigurasi-instrumen.vue
+++ pages/admin/konfigurasi-instrumen.vue
@@ -5,55 +5,84 @@
     </h1>
 
     <template v-for="(group, index) in groupedData" :key="index">
-      <Table
-        class="bg-white overflow-x-auto mb-4 overflow-hidden border-separate"
-      >
+      <Table class="overflow-x-auto mb-4 overflow-hidden border-separate">
         <TableBody>
-          <!-- Loop Domain Group -->
-          <!-- Header Domain -->
+          <!-- Header Domain (1x saja per domain) -->
           <TableRow
-            class="hover:bg-slate-800 bg-slate-800 text-slate-50 font-semibold"
+            class="hover:bg-slate-800 text-slate-50 font-semibold rounded-sm"
           >
-            <TableCell colspan="9">
-              Domain {{ index + 1 }} : {{ group.domain }}
+            <TableCell colspan="9" class="bg-slate-800 rounded-sm">
+              Domain {{ index + 1 }}: {{ group.domain.nama }}
             </TableCell>
           </TableRow>
 
-          <!-- Table Header -->
-          <TableRow class="bg-gray-200 text-sm text-gray-600 hover:bg-gray-200">
-            <TableHead>Indikator</TableHead>
-            <TableHead class="w-8/12">Penjelasan</TableHead>
-            <TableHead class="text-center">Indeks</TableHead>
-            <TableHead class="text-center">Target</TableHead>
-            <TableHead class="text-center">Satker</TableHead>
-            <TableHead>Substansi</TableHead>
-            <TableHead class="w-4">Tindak Lanjut 2024</TableHead>
-            <TableHead class="text-center">Aksi</TableHead>
-          </TableRow>
-
-          <!-- Rows per Indikator -->
-          <TableRow
-            v-for="(item, i) in group.items"
-            :key="i"
-            class="hover:bg-muted/200"
+          <!-- Loop each Aspek inside this Domain -->
+          <template
+            v-for="(aspek, aspekIndex) in group.domain.aspek"
+            :key="aspekIndex"
           >
-            <TableCell>{{ item.indikator }}</TableCell>
-            <TableCell>{{ item.penjelasan }}</TableCell>
-            <TableCell class="text-center">{{ item.indeks }}</TableCell>
-            <TableCell>{{ item.target }}</TableCell>
-            <TableCell>{{ item.satker }}</TableCell>
-            <TableCell>{{ item.substansi }}</TableCell>
-            <TableCell class="w-4 text-wrap">{{ item.tindakLanjut }}</TableCell>
-            <TableCell class="text-center">
-              <Button
-                size="sm"
-                @click="handleEdit(item, group.domain)"
-                class="cursor-pointer"
+            <!-- Header Aspek -->
+            <TableRow class="text-slate-50 font-semibold">
+              <TableCell
+                colspan="9"
+                class="hover:bg-slate-700 bg-slate-600 rounded-sm"
               >
-                Edit
-              </Button>
-            </TableCell>
-          </TableRow>
+                Aspek {{ aspekIndex + 1 }}. {{ aspek.nama }}
+              </TableCell>
+            </TableRow>
+
+            <!-- Table Header -->
+            <TableRow
+              class="bg-slate-100 text-sm text-gray-600 hover:bg-gray-200"
+            >
+              <TableHead>Indikator</TableHead>
+              <TableHead class="w-8/12">Penjelasan</TableHead>
+              <TableHead class="text-center">Indeks</TableHead>
+              <TableHead class="text-center">Target</TableHead>
+              <TableHead class="text-center">Satker</TableHead>
+              <TableHead>Substansi</TableHead>
+              <TableHead class="w-4">Tindak Lanjut 2024</TableHead>
+              <TableHead class="text-center">Aksi</TableHead>
+            </TableRow>
+
+            <!-- Rows per Indikator -->
+            <TableRow
+              v-for="(item, i) in aspek.items"
+              :key="i"
+              class="hover:bg-muted/200"
+            >
+              <TableCell class="rounded-sm bg-white">{{
+                item.indikator
+              }}</TableCell>
+              <TableCell class="rounded-sm bg-white">{{
+                item.penjelasan
+              }}</TableCell>
+              <TableCell class="rounded-sm bg-white text-center">{{
+                item.indeks
+              }}</TableCell>
+              <TableCell class="rounded-sm bg-white text-center">{{
+                item.target
+              }}</TableCell>
+              <TableCell class="rounded-sm bg-white">{{
+                item.satker
+              }}</TableCell>
+              <TableCell class="rounded-sm bg-white text-center">{{
+                item.substansi
+              }}</TableCell>
+              <TableCell class="rounded-sm bg-white w-4 text-wrap">{{
+                item.tindakLanjut
+              }}</TableCell>
+              <TableCell class="rounded-sm bg-white text-center">
+                <Button
+                  size="sm"
+                  @click="handleEdit(item, group.domain.nama)"
+                  class="cursor-pointer"
+                >
+                  Edit
+                </Button>
+              </TableCell>
+            </TableRow>
+          </template>
         </TableBody>
       </Table>
     </template>
@@ -166,50 +195,61 @@ const handleSave = () => {
 // Grouped Data: per domain
 const groupedData = [
   {
-    domain: "Kebijakan Internal SPBE",
-    items: [
-      {
-        indikator: "Indikator 1",
-        penjelasan: "Tingkat Kematangan Kebijakan Internal Arsitektur SPBE",
-        indeks: 3,
-        target: 5,
-        satker: "PUSDATIN",
-        substansi: "SPBE",
-        tindakLanjut: "Kepmen Arsitektur 303/M/2024",
-      },
-      {
-        indikator: "Indikator 2",
-        penjelasan: "Tingkat Kematangan Kebijakan Internal Peta Rencana SPBE",
-        indeks: 3,
-        target: 5,
-        satker: "PUSDATIN",
-        substansi: "SPBE",
-        tindakLanjut: "Kepmen Peta Rencana (Draft)",
-      },
-    ],
-  },
-  {
-    domain: "Tata Kelola Data",
-    items: [
-      {
-        indikator: "Indikator 3",
-        penjelasan: "Tingkat Kematangan Kebijakan Internal Manajemen Data",
-        indeks: 3,
-        target: 4,
-        satker: "PUSDATIN",
-        substansi: "Data Pendidikan",
-        tindakLanjut: "Kepmen Arsitektur SPBE",
-      },
-      {
-        indikator: "Indikator 4",
-        penjelasan: "Tingkat Kematangan Kebijakan Internal Audit TIK",
-        indeks: 4,
-        target: 5,
-        satker: "PUSDATIN",
-        substansi: "PAKI",
-        tindakLanjut: "SK Pendampingan Audit Eksternal (KAPUS)",
-      },
-    ],
+    domain: {
+      nama: "Kebijakan Internal SPBE",
+      aspek: [
+        {
+          nama: "Kebijakan Internal Tata Kelola",
+          items: [
+            {
+              indikator: "Indikator 1",
+              penjelasan:
+                "Tingkat Kematangan Kebijakan Internal Arsitektur SPBE",
+              indeks: 3,
+              target: 5,
+              satker: "PUSDATIN",
+              substansi: "SPBE",
+              tindakLanjut: "Kepmen Arsitektur 303/M/2024",
+            },
+            {
+              indikator: "Indikator 2",
+              penjelasan:
+                "Tingkat Kematangan Kebijakan Internal Peta Rencana SPBE",
+              indeks: 3,
+              target: 5,
+              satker: "PUSDATIN",
+              substansi: "SPBE",
+              tindakLanjut: "Kepmen Peta Rencana (Draft) 2024 2025",
+            },
+          ],
+        },
+        {
+          nama: "Perencanaan Strategis SPBE",
+          items: [
+            {
+              indikator: "Indikator 1",
+              penjelasan:
+                "Tingkat Kematangan Kebijakan Internal Arsitektur SPBE",
+              indeks: 3,
+              target: 5,
+              satker: "PUSDATIN",
+              substansi: "SPBE",
+              tindakLanjut: "Kepmen Arsitektur 303/M/2024",
+            },
+            {
+              indikator: "Indikator 2",
+              penjelasan:
+                "Tingkat Kematangan Kebijakan Internal Peta Rencana SPBE",
+              indeks: 3,
+              target: 5,
+              satker: "PUSDATIN",
+              substansi: "SPBE",
+              tindakLanjut: "Kepmen Peta Rencana (Draft)",
+            },
+          ],
+        },
+      ],
+    },
   },
 ];
 
